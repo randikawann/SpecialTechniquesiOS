1bf9689d7393910a7de54f08658808c700e69bee
randika <randikawann@gmail.com>
2020-11-09 17:48:13 -0800

    Successfully edit autolayout function with programetically

---
+++ SpecialTechniquesiOS/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SpecialTechniquesiOS
+//
+//  Created by Randika Wanninayaka on 11/9/2563 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
